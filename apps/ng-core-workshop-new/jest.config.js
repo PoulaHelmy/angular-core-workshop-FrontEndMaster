b00382e3da3e48a9d573b49482647c12a3596722
PoulaHelmy <bolahelmy2016@gmail.com>
2020-08-25 14:08:18 +0200

    initial commit

---
+++ apps/ng-core-workshop-new/jest.config.js
@@ -0,0 +1,21 @@
+module.exports = {
+  name: 'ng-core-workshop-new',
+  preset: '../../jest.config.js',
+  setupFilesAfterEnv: ['<rootDir>/src/test-setup.ts'],
+  globals: {
+    'ts-jest': {
+      tsConfig: '<rootDir>/tsconfig.spec.json',
+      stringifyContentPathRegex: '\\.(html|svg)$',
+      astTransformers: [
+        'jest-preset-angular/build/InlineFilesTransformer',
+        'jest-preset-angular/build/StripStylesTransformer',
+      ],
+    },
+  },
+  coverageDirectory: '../../coverage/apps/ng-core-workshop-new',
+  snapshotSerializers: [
+    'jest-preset-angular/build/AngularNoNgAttributesSnapshotSerializer.js',
+    'jest-preset-angular/build/AngularSnapshotSerializer.js',
+    'jest-preset-angular/build/HTMLCommentSerializer.js',
+  ],
+};
---
+++ apps/ng-core-workshop-new/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'angular-core-workplace-new-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss'],
+})
+export class AppComponent {
+  title = 'ng-core-workshop-new';
+}
