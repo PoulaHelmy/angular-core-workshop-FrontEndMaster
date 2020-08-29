fd2739c6f5c7f8ceb04881845536c4004c95eee2
PoulaHelmy <bolahelmy2016@gmail.com>
2020-08-29 04:37:15 +0200

    create routing & navigation

--- apps/ng-core-workshop-new/src/app/app-routing.module.ts
+++ apps/ng-core-workshop-new/src/app/app-routing.module.ts
@@ -1,11 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from '../../../../libs/ui-login/src/lib/login/login.component';
 
 const routes: Routes = [
   { path: '', loadChildren: () => import('./home/home.module').then(m => m.HomeModule) },
   { path: 'projects', loadChildren: () => import('./projects/projects.module').then(m => m.ProjectsModule) },
   { path: 'customers', loadChildren: () => import('./customers/customers.module').then(m => m.CustomersModule) },
-  // { path: 'login', component: LoginComponent }
+  { path: 'login', component: LoginComponent }
 ];
 
 @NgModule({
