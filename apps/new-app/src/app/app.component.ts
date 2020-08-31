fa5b4882c44f1507b855c96ca4f9dff2b81449f8
PoulaHelmy <bolahelmy2016@gmail.com>
2020-08-31 11:45:42 +0200

    add new app

---
+++ apps/new-app/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'angular-core-workplace-new-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss'],
+})
+export class AppComponent {
+  title = 'new-app';
+}
